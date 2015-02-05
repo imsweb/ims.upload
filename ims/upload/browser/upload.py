baf5c4c34a85442cfa9652a1c14148ac729c3ded
Eric Wohnlich <wohnlice@imsweb.com>
2015-02-05 17:57:54 +0000

    UI tweaks

--- ims/upload/browser/upload.py
+++ ims/upload/browser/upload.py
@@ -3,12 +3,13 @@ from five import grok
 
 from plone.namedfile.file import NamedBlobFile
 from plone.registry.interfaces import IRegistry
+from Products.CMFCore.utils import getToolByName
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from tempfile import NamedTemporaryFile
 from zope.component import getAllUtilitiesRegisteredFor, getUtility
 from zope.filerepresentation.interfaces import IFileFactory
 
-from ims.upload.interfaces import IChunkSettings, IFileMutator, IUploadCapable
+from ims.upload.interfaces import IChunkSettings, IFileMutator, IUploadCapable, IChunkedFile
 
 import logging
 logger = logging.getLogger('ims.upload')
@@ -28,6 +29,11 @@ class ChunkUploadView(grok.View):
         request=self.request
 
       return self.listing()
+    
+    def chunked_files(self):
+      catalog = getToolByName(self.context,'portal_catalog')
+      return catalog(path={'query':'/'.join(self.context.getPhysicalPath()),'depth':1},
+                     portal_type='ChunkedFile')
 
     def chunksize(self):
         registry = getUtility(IRegistry).forInterface(IChunkSettings)
@@ -134,7 +140,67 @@ class ChunkCheck(grok.View):
     
     def render(self):
       file_name = self.request.form['file']
-      data = 0
+      data = {'uploadedBytes':0}
       if file_name + '_chunk' in self.context.objectIds():
-        data = self.context[file_name + '_chunk'].currsize()
+        data['uploadedBytes'] = self.context[file_name + '_chunk'].currsize()
+        data['url'] = self.context[file_name + '_chunk'].absolute_url()
       return json.dumps(data)
+
+class ChunkCheckDirect(grok.View):
+    """ Upload form page """
+    grok.name('chunk-check')
+    grok.context(IChunkedFile)
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+    
+    def render(self):
+      #file_name = self.request.form['file']
+      data = {'uploadedBytes':0}
+      data['uploadedBytes'] = self.context.currsize()
+      return json.dumps(data)
+
+class ChunkedUploadDirect(grok.View):
+    """ Upload a file
+    """
+    grok.name('upload-chunk')
+    grok.context(IChunkedFile)
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def render(self):
+      _files = {}
+      file_data = self.request.form['files[]']
+      file_name = file_data.filename
+      file_name = file_name.split('/')[-1].split('\\')[-1] # bug in old IE
+      _file_name = file_name+'_chunk'
+
+      chunk_size = self.request['CONTENT_LENGTH']
+      content_range = self.request['HTTP_CONTENT_RANGE']
+
+      content_type = mimetypes.guess_type(file_name)[0] or ""
+      for mutator in getAllUtilitiesRegisteredFor(IFileMutator):
+          file_name, file_data, content_type = mutator(file_name, file_data, content_type)
+
+      complete = False
+      if content_range:
+        """ don't do anything special if we only have one chunk """
+        max_size = int(content_range.split('/')[-1])
+
+        if file_data:
+          self.context.addChunk(file_data,file_name,content_range,graceful=True)
+
+          size = self.context.currsize()
+          url = self.context.absolute_url()
+          _files[file_name]= {'name':file_name,
+                              'size':size,
+                              'url':url}
+
+          if size == max_size :
+            logger.info('Starting chunk merger')
+            nf_url = mergeChunks(self.context.aq_parent, self.context, file_name)
+            complete = self.context.aq_parent.absolute_url() + '/@@upload'
+      return json.dumps({'files':_files.values(),'complete':complete})
--- ims/upload/content.py
+++ ims/upload/content.py
@@ -1,14 +1,17 @@
 from five import grok
+import math
 from plone.dexterity.content import Item, Container
 from plone.directives.dexterity import DisplayForm
 from plone.namedfile.file import NamedBlobFile
+from plone.registry.interfaces import IRegistry
 from Products.CMFPlone.utils import safe_unicode as su
+from zope.component import getAllUtilitiesRegisteredFor, getUtility
 import re
 
 import logging
 logger = logging.getLogger('ims.upload')
 
-from interfaces import IChunkedFile, IChunk
+from interfaces import IChunkedFile, IChunk, IChunkSettings
 
 grok.templatedir('browser')
 
@@ -18,10 +21,28 @@ class ChunkedFile(Container):
     """
 
     def currsize(self):
-        return sum([c.file.getSize() for c in self.objectValues()])
+        """ Get the size up until the first missing chunk """
+        chunks = sorted(self.objectValues(),key=lambda term: term.startbyte)
+        registry = getUtility(IRegistry).forInterface(IChunkSettings)
+        chunksize = registry.chunksize
+          
+        # check for missing chunks:
+        counter = 0
+        sum = 0
+        for chunk in chunks:
+          if chunk.startbyte != counter:
+            return counter
+          counter += chunksize
+          sum += chunk.file.getSize()
+        return sum
    
-    def addChunk(self,file_data,file_name,content_range):
+    def addChunk(self,file_data,file_name,content_range,graceful=False):
+        if not self.targetsize:
+          self.targetsize = content_range.split('/')[-1]
         id = content_range.replace(' ','_').replace('/',' of ') or file_name # just use file name if only one chunk
+        if id in self.objectIds() and graceful:
+          logger.info('Chunk already exists: %s; assume file resume' % file_name)
+          return
         self.invokeFactory('Chunk',id)
         chunk = self[id]
         chunk.file = NamedBlobFile(file_data.read(),filename = su(file_name))
@@ -32,13 +53,30 @@ class ChunkedFile(Container):
         logger.info('Chunk uploaded: %s; %s' % (content_range,file_name))
     
     def Title(self):
-        return 'Processing/Aborted - ' + self.id[:-1]
+        return 'Processing/Aborted - ' + self.id[:-6] # remove _chunk from id
 
 class ChunkedFileView(DisplayForm):
     grok.context(IChunkedFile)
     grok.name('chunkedfile-view')
     grok.template('chunkedfile-view')
 
+    def chunksize(self):
+        registry = getUtility(IRegistry).forInterface(IChunkSettings)
+        return registry.chunksize
+    
+    def printable_size(self, fsize):
+        if fsize == 0:
+          return '0 B'
+        prefixes = ['B','KB','MB','GB','TB','PB']
+        tens = int(math.log(fsize,1024))
+        fsize = round(fsize/math.pow(1024,tens),2)
+        if tens < len(prefixes):
+          return '%.2f %s' % (fsize,prefixes[tens])
+        else: # uhhhh, we should never have a file this big
+          return '%.2f %s' % (fsize * 1024 ** tens,'B')
+
+    def currsize(self):
+        return '%s of %s' % (self.printable_size(self.context.currsize()),self.context.targetsize and self.printable_size(int(self.context.targetsize) or '0 B'))
 
 class Chunk(Item):
     """ An individual chunk """
