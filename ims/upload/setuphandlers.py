fef4b92c6d0515b81964ae983e12edcf192ea3e3
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2015-09-09 14:44:42 -0400

    put add chunked file permission in GroupSpace workflow

---
+++ ims/upload/setuphandlers.py
@@ -0,0 +1,18 @@
+from Products.CMFCore.utils import getToolByName
+
+def setup_various(context):
+  """Miscellanous steps import handle
+  """
+
+  portal = context.getSite()
+  qi = getToolByName(portal,'portal_quickinstaller')
+  setup = getToolByName(portal,'portal_setup')
+  pw = getToolByName(portal,'portal_workflow')
+  if qi.isProductInstalled('CRNTracker'):
+    # redo the workflow step to allow in GroupSpaces
+    setup.runImportStepFromProfile('profile-Products.CRNTracker:default', 'workflow')
+    pw.updateRoleMappings()
+  elif qi.isProductInstalled('GroupSpace'):
+    # redo the workflow step to allow in GroupSpaces
+    setup.runImportStepFromProfile('profile-Products.GroupSpace:default', 'workflow')
+    pw.updateRoleMappings()
