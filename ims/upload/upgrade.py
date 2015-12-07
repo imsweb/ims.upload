d722c0002f518ce019410740128731795229a377
Wohnlich, Eric (IMS) <wohnliche@imsweb.com>
2015-12-07 14:48:09 -0500

    don't get portal this way

--- ims/upload/upgrade.py
+++ ims/upload/upgrade.py
@@ -7,12 +7,10 @@ from zope.component import getUtility
 import logging, transaction
 
 def to_1_5(context, logger=None):
-  portal = getUtility(ISiteRoot, context=context)
   if logger is None:
       logger = logging.getLogger('ims.upload')
-  qi = getToolByName(portal,'portal_quickinstaller')
+  qi = getToolByName(context,'portal_quickinstaller')
   if qi.isProductInstalled('ims.upload'):
     PROFILE_ID = 'profile-ims.upload:default'
-    setup_tool = getToolByName(portal,'portal_setup')
-    setup_tool.runImportStepFromProfile(PROFILE_ID, 'actions')
+    context.runImportStepFromProfile(PROFILE_ID, 'actions')
     logger.info("Updated upload action")
